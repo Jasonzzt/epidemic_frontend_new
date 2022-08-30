32daf7cdd5b188a94287b1cb40b954da31e13a36
lychee <1007258830@qq.com>
2022-08-30 15:55:33 +0800

    张莹8.30 15.55

--- src/views/PredictionPage.vue
+++ src/views/PredictionPage.vue
@@ -290,8 +290,8 @@ export default {
               0
             ],
             bottom: 10,
-            start: 10,
-            end: 80,
+            start: 50,
+            end: 100,
             handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
             handleSize: '110%',
             handleStyle: {
